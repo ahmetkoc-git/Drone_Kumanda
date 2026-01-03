267596bc03379420bbadc245f222aaa33e91185b
ahmetkoc-git <ahmet.nabikoc55@gmail.com>
2026-01-03 19:40:17 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -22,6 +22,7 @@
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 #include "joystick.h"
+#include "nrf24l01p.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -43,16 +44,21 @@
 ADC_HandleTypeDef hadc1;
 DMA_HandleTypeDef hdma_adc1;
 
+SPI_HandleTypeDef hspi1;
+
 /* USER CODE BEGIN PV */
 ///////////////////Joystick pvs////////////////////////////////
 int8_t ROLL_joystick;
 int8_t PITCH_joystick;
+int8_t YAW_joystick;
+int8_t ALTITUDE_joystick;
 uint16_t X;
 uint16_t Y;
 uint16_t Xx;
 uint16_t Yy;
-int yaw;
-int updown;
+///////////////////NRF24L01P PVS///////////////////////////////
+uint8_t Status_NRF;
+uint8_t SendBuffer[4];
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -60,6 +66,7 @@ void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_DMA_Init(void);
 static void MX_ADC1_Init(void);
+static void MX_SPI1_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -100,10 +107,15 @@ int main(void)
   MX_GPIO_Init();
   MX_DMA_Init();
   MX_ADC1_Init();
+  MX_SPI1_Init();
   /* USER CODE BEGIN 2 */
 ///////////////////JOYSTICK CODES//////////////////////////////
 JoystickInit(&hadc1);
+HAL_Delay(50);//cop degerler gelmesin joystickler hazir olsun.
+getADCValues(&X, &Y,&Xx,&Yy);
 
+////////////////////NRF24L01P CODES////////////////////////////
+nrf24l01p_tx_init(2, _1Mbps);
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -114,8 +126,17 @@ JoystickInit(&hadc1);
 
     /* USER CODE BEGIN 3 */
 	  ////////////////////JOYSTICK CODES////////////////////////////
-	  getADCValues(&X, &Y,&Xx,&Yy);
-	  ConvertToAngle(&ROLL_joystick, &PITCH_joystick);
+      ConvertToAngle(&ROLL_joystick, &PITCH_joystick, &YAW_joystick, &ALTITUDE_joystick,X,Y,Xx,Yy);
+
+      ////////////////////NRF24L01P CODES/////////////////////////////////////////////////////////
+      Status_NRF = nrf24l01p_get_status();
+      SendBuffer[0]=(uint8_t)ROLL_joystick;
+      SendBuffer[1]=(uint8_t)PITCH_joystick;
+      SendBuffer[2]=(uint8_t)YAW_joystick;
+      SendBuffer[3]=(uint8_t)ALTITUDE_joystick;
+      for (int i = 0; i <= 3; ++i) {//ayri bir rf kodu olusturup txfifoyu temizleme kodu eklenecek.
+    	  nrf24l01p_tx_transmit(&SendBuffer[i]);
+	}
 
   }
   /* USER CODE END 3 */
@@ -239,6 +260,44 @@ static void MX_ADC1_Init(void)
 }
 
 /**
+  * @brief SPI1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_SPI1_Init(void)
+{
+
+  /* USER CODE BEGIN SPI1_Init 0 */
+
+  /* USER CODE END SPI1_Init 0 */
+
+  /* USER CODE BEGIN SPI1_Init 1 */
+
+  /* USER CODE END SPI1_Init 1 */
+  /* SPI1 parameter configuration*/
+  hspi1.Instance = SPI1;
+  hspi1.Init.Mode = SPI_MODE_MASTER;
+  hspi1.Init.Direction = SPI_DIRECTION_2LINES;
+  hspi1.Init.DataSize = SPI_DATASIZE_8BIT;
+  hspi1.Init.CLKPolarity = SPI_POLARITY_LOW;
+  hspi1.Init.CLKPhase = SPI_PHASE_1EDGE;
+  hspi1.Init.NSS = SPI_NSS_SOFT;
+  hspi1.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_2;
+  hspi1.Init.FirstBit = SPI_FIRSTBIT_MSB;
+  hspi1.Init.TIMode = SPI_TIMODE_DISABLE;
+  hspi1.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
+  hspi1.Init.CRCPolynomial = 10;
+  if (HAL_SPI_Init(&hspi1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN SPI1_Init 2 */
+
+  /* USER CODE END SPI1_Init 2 */
+
+}
+
+/**
   * Enable DMA controller clock
   */
 static void MX_DMA_Init(void)
@@ -261,12 +320,24 @@ static void MX_DMA_Init(void)
   */
 static void MX_GPIO_Init(void)
 {
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
   /* USER CODE BEGIN MX_GPIO_Init_1 */
 
   /* USER CODE END MX_GPIO_Init_1 */
 
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, ce_Pin|nss_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pins : ce_Pin nss_Pin */
+  GPIO_InitStruct.Pin = ce_Pin|nss_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
   /* USER CODE BEGIN MX_GPIO_Init_2 */
 
