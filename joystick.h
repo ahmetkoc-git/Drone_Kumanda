6c0cc521548390f9e8c2fd39c5ef5170b125ccd4
ahmetkoc-git <ahmet.nabikoc55@gmail.com>
2026-01-03 19:41:17 +0300

    Add files via upload

--- joystick.h
+++ joystick.h
@@ -13,7 +13,7 @@
 
 void JoystickInit(ADC_HandleTypeDef *hadcx);
 void getADCValues(uint16_t *XAxisValue, uint16_t *YAxisValue,uint16_t *XxAxisValue,uint16_t *YyAxisValue);
-void ConvertToAngle(int8_t *roll, int8_t *pitch);
+void ConvertToAngle(int8_t *roll, int8_t *pitch, int8_t *yaw, int8_t *altitude,uint16_t x,uint16_t y,uint16_t xx,uint16_t yy);
 void SecondJoystickGetADC(ADC_HandleTypeDef *hadcx,int *yaw,int *updown, uint16_t *Xx,uint16_t *Yy);
 
 #endif /* INC_JOYSTICK_H_ */
